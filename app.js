ec85cadbf97013c4d44a6264ab6d8c6c3e407532
alexx <alexx1984@ukr.net>
2021-06-13 11:46:48 +0300

    befor home work finish express

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,12 +9,13 @@ const  adminRouter = require('./routes/admin');
 const  shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.listen(3002);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
